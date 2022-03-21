59927070ecc9d8daf3ea2efeef01978fa92a072b
stephenkgh <stephenkgh@stephenk.com>
2022-03-21 15:25:28 -0400

    static files working
    - all images/css/fonts/etc working in production
    - moved to stephenk.com
    - needed separate production requirements file for python deps
    - staticfiles export now working
    - more links fixing (favicon, css urls, theme form)

--- molinks/settings.py
+++ molinks/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-axby@$2^_skosd3k@-nxx7wo0)0uyqgnq=e&5g9*@@iq&)6=r3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'killermartian.com']
+ALLOWED_HOSTS = ['localhost', 'stephenk.com']
 
 # Application definition
 
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
