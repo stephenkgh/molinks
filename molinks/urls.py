ad1733676520879b17d0a715d4a3f79b60464431
stephenkgh <stephenkgh@stephenk.com>
2022-03-23 11:24:02 -0400

    bugfix: theme url broken from recent view

--- molinks/urls.py
+++ molinks/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('recent/', views.recent, name='recent'),
     path('link/<int:link_id>', views.edit_link, name='edit_link'),
     path('category/<int:cat_id>', views.edit_cat, name='edit_cat'),
-    path('theme/', views.choose_theme, name='choose_theme'),
+    path('theme/', views.choose_theme, name='theme'),
 
     # login management
     path('login/', loginviews.login, name='login'),
